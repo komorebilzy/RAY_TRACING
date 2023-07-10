c7b30b0fd71df024872002533354ef2e70ef8fb3
komorebilz <2110429490@qq.com>
2023-07-10 22:12:09 +0800

    book2_image2

--- raytracer/src/hittablelist.rs
+++ raytracer/src/hittablelist.rs
@@ -54,19 +54,3 @@ impl Hittable for HitableList {
         Some(output_box)
     }
 }
-
-// impl Hittable for HitableList {
-//     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
-//         let mut hit_anything: bool = false;
-//         let mut closest_so_far: f64 = t_max;
-//         let mut temp_rec = HitRecord::new();
-//         for i in self.objects.clone() {
-//             if i.hit(r, t_min, closest_so_far, &mut temp_rec) {
-//                 hit_anything = true;
-//                 closest_so_far = temp_rec.t;
-//                 *rec = temp_rec;
-//             }
-//         }
-//         hit_anything
-//     }
-// }
