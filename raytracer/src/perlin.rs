df7243f1e4e2bbe1aa1ae23e0efc8985c207242b
komorebilz <2110429490@qq.com>
2023-07-12 11:48:43 +0800

    book2_image11

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -2,21 +2,26 @@ use crate::*;
 // use rand::prelude::SliceRandom;
 pub struct Perlin {
     pub point_count: i64,
-    pub ranfloat: Vec<f64>,
+    // pub ranfloat: Vec<f64>,
+    ranvec: Vec<Vect3>,
     pub perm_x: Vec<i64>,
     pub perm_y: Vec<i64>,
     pub perm_z: Vec<i64>,
 }
 impl Perlin {
-    pub fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    pub fn trilinear_interp(c: [[[Vect3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
         let mut accum = 0.0;
         for (i, sub) in c.iter().enumerate() {
             for (j, subsub) in sub.iter().enumerate() {
                 for (k, value) in subsub.iter().enumerate() {
-                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
-                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
-                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
-                        * value;
+                    let weight_v = Vect3::new(u - i as f64, v - j as f64, w - k as f64);
+                    accum += dot(*value, weight_v)
+                        * (i as f64 * uu + (1.0 - i as f64) * (1.0 - uu))
+                        * (j as f64 * vv + (1.0 - j as f64) * (1.0 - vv))
+                        * (k as f64 * ww + (1.0 - k as f64) * (1.0 - ww));
                 }
             }
         }
@@ -31,20 +36,26 @@ impl Perlin {
     //         [(self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]) as usize]
     // }
     pub fn noise(&self, p: Vect3) -> f64 {
-        let mut u = p.x() - p.x().floor();
-        let mut v = p.y() - p.y().floor();
-        let mut w = p.z() - p.z().floor();
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
         let i = p.x().floor() as i64;
         let j = p.y().floor() as i64;
         let k = p.z().floor() as i64;
-        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0, 0.0], [0.0, 0.0]], [[0.0, 0.0], [0.0, 0.0]]];
+        let mut c: [[[Vect3; 2]; 2]; 2] = [
+            [
+                [Vect3::default(), Vect3::default()],
+                [Vect3::default(), Vect3::default()],
+            ],
+            [
+                [Vect3::default(), Vect3::default()],
+                [Vect3::default(), Vect3::default()],
+            ],
+        ];
         for (di, sub) in c.iter_mut().enumerate() {
             for (dj, subsub) in sub.iter_mut().enumerate() {
                 for (dk, value) in subsub.iter_mut().enumerate() {
-                    *value = self.ranfloat[(self.perm_x[((i + di as i64) & 255) as usize]
+                    *value = self.ranvec[(self.perm_x[((i + di as i64) & 255) as usize]
                         ^ self.perm_y[((j + dj as i64) & 255) as usize]
                         ^ self.perm_z[((k + dk as i64) & 255) as usize])
                         as usize];
@@ -72,13 +83,13 @@ impl Perlin {
     }
 
     pub fn new() -> Self {
-        let mut ran: Vec<f64> = Vec::new();
+        let mut ran: Vec<Vect3> = Vec::new();
         for _i in 0..256 {
-            ran.push(random_double());
+            ran.push(unit_vector(Vect3::random1(-1.0, 1.0)));
         }
         Self {
             point_count: (256),
-            ranfloat: (ran),
+            ranvec: (ran),
             perm_x: Perlin::perlin_generate_perm(),
             perm_y: Perlin::perlin_generate_perm(),
             perm_z: Perlin::perlin_generate_perm(),
